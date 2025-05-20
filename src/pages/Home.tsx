1ad034601136b6b48bb6ab5a74d7373c9a575e09
Favour Vivian Woka <vivianemma97@gmail.com>
2025-05-20 01:49:03 +0100

    set up project, design home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,62 @@
+/** @format */
+
+import styled from "styled-components";
+import lady from "../assets/4.mp4";
+import logo from "../assets/logo.png";
+
+const PageWrapper = styled.div`
+  width: 100%;
+  overflow-x: hidden;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1.5rem 2rem;
+  background-color: #545454;
+  color: #fff;
+
+  img {
+    width: 100px;
+  }
+`;
+
+const VideoSection = styled.section`
+  width: 100%;
+  box-sizing: border-box;
+
+  video {
+    width: 100%;
+    max-height: 100%;
+    object-fit: cover;
+  }
+
+  @media (max-width: 768px) {
+    video {
+      max-height: 50vh;
+    }
+  }
+`;
+
+function Home() {
+  return (
+    <PageWrapper>
+      <Nav>
+        <img src={logo} alt="logo" />
+        <button className="transition-all duration-500 transform hover:scale-105 hover:-rotate-1">
+          Upload
+        </button>
+      </Nav>
+
+      <VideoSection>
+        <video autoPlay muted className="video-background">
+          <source src={lady} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </VideoSection>
+    </PageWrapper>
+  );
+}
+
+export default Home;
